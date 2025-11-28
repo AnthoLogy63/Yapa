da9a6212896e04d332426c526bd363bfcfbdf174
AjraHuacsoJeansAnthony <jajra@unsa.edu.pe>
2025-11-28 11:57:09 -0500

    Restaurando cambios

---
+++ frontend/src/api/authApi.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_URL = 'http://127.0.0.1:8000/api/auth/';
+
+export const loginWithGoogle = async (accessToken) => {
+  try {
+    const res = await axios.post(`${API_URL}google/`, {
+      access_token: accessToken
+    });
+    return res.data; 
+  } catch (err) {
+    console.error('Error logueando con backend:', err);
+    throw err;
+  }
+};
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { AuthProvider } from "./context/AuthContext.jsx"; 
+
+const GOOGLE_CLIENT_ID = "877945333888-jb8qd73ai3cr56omv75uiq5gnijdd8kj.apps.googleusercontent.com";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <AuthProvider> 
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
+    </GoogleOAuthProvider>
   </React.StrictMode>
-)
+);
