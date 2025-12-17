34a77b803b224ecaec934cc9a5f49a3029fd215b
AjraHuacsoJeansAnthony <jajra@unsa.edu.pe>
2025-12-17 09:02:56 -0500

    Diseño general

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -2,18 +2,20 @@ import YapaLogo from '../../assets/logo.png';
 
 function Footer() {
   return (
-    <div className="w-full bg-white pt-4 mt-5 flex flex-col items-center">
-      
-      <div className="w-[40vw] h-1 bg-[#F99F3F]"></div>
-
-      <div className="flex flex-col items-center justify-center p-8">
-        <img 
-          src={YapaLogo} 
-          alt="Logo Yapa" 
-          className="w-45 h-auto" 
+    <div className="w-full bg-[#F99F3F] pt-6 mt-16 flex flex-col items-center">
+      <div className="flex items-center justify-center p-12">
+        <img
+          src={YapaLogo}
+          alt="Logo Yapa"
+          className="
+            w-44 h-auto
+            drop-shadow-[2px_0_0_white]
+            drop-shadow-[-2px_0_0_white]
+            drop-shadow-[0_2px_0_white]
+            drop-shadow-[0_-2px_0_white]
+          "
         />
       </div>
-      
     </div>
   );
 }
